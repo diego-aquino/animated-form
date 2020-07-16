fcd9a63a159da2988a39f2dac18f4d123f52e98c
diego-aquino <diego.cruz001@gmail.com>
2020-07-16 19:20:15 -0300

    Implement random creation of background squares

--- main.js
+++ main.js
@@ -35,3 +35,43 @@ function handleAnimationEnd({ animationName }) {
         form.classList.remove('reject');
     }
 }
+
+createBackgroundSquares();
+
+function createBackgroundSquares() {
+    const squareContainer = document.querySelector('ul.square-container');
+
+    const numOfSquares = randomInt(15, 25);
+
+    for (let i = 0; i < numOfSquares; i++) {
+        const square = document.createElement('li');
+
+        const size = randomInt(40, 100);
+        const positionX = randomInt(0, 99);
+        const positionY = - size;
+
+        square.style.width = `${size}px`;
+        square.style.height = `${size}px`;
+
+        square.style.bottom = `${positionY}px`;
+        square.style.left = `${positionX}%`;
+
+        const opacity = randomInt(20, 50);
+
+        square.style.opacity = `${opacity / 100}`;
+
+        const delay = randomInt(0, 30);
+        const duration = randomInt(15, 35);
+
+        square.style.animationDuration = `${duration}s`;
+        square.style.animationDelay = `${delay}s`;
+        square.style.animationTimingFunction = 'linear';
+
+        squareContainer.appendChild(square);
+    }
+}
+
+function randomInt(start, end) {
+    // random integer from start to end (inclusive)
+    return Math.round(Math.random() * (end - start)) + start;
+}
